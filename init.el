a5750da8fa58a40c1a6ab824aec4a783d0de7b55
nahso <topcloud@foxmail.com>
2022-12-29 12:33:04 +0800

    lookings

--- init.el
+++ init.el
@@ -17,14 +17,19 @@
 (maybe-require-package 'scratch)
 (require-package 'command-log-mode)
 
+(require 'init-frame-hooks)
+(require 'init-theme)
+(require 'init-gui-frames)
+
 (provide 'init)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(spacemacs-dark))
  '(package-selected-packages
-   '(command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq)))
+   '(disable-mouse default-text-scale spacemacs-theme command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ lisp/init-theme.el
@@ -0,0 +1,20 @@
+(require-package 'spacemacs-theme)
+
+;; Don't prompt to confirm theme safety. This avoids problems with
+;; first-time startup on Emacs > 26.3.
+(setq custom-safe-themes t)
+
+;; If you don't customize it, this is the theme you get.
+(setq-default custom-enabled-themes '(spacemacs-dark))
+
+;; Ensure that themes will be applied even if they have not been customized
+(defun reapply-themes ()
+  "Forcibly load the themes listed in `custom-enabled-themes'."
+  (dolist (theme custom-enabled-themes)
+    (unless (custom-theme-p theme)
+      (load-theme theme)))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+
+(add-hook 'after-init-hook 'reapply-themes)
+
+(provide 'init-theme)
