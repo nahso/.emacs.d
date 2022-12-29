82242301b08f7796446e1c25dd7442749794e95a
nahso <topcloud@foxmail.com>
2022-12-29 12:23:36 +0800

    package install: diminish, scratch, command-and-log

--- init.el
+++ init.el
@@ -12,4 +12,22 @@
 (require 'init-elpa)
 (require 'init-exec-path) ;; Set up $PATH
 
+;; Load configs for specific features and modes
+(require-package 'diminish)
+(maybe-require-package 'scratch)
+(require-package 'command-log-mode)
+
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
