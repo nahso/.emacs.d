e45490a5e3b35d8f112b47032d3559c52a5b05a5
nahso <topcloud@foxmail.com>
2023-01-25 16:55:22 +0800

    2

--- init.el
+++ init.el
@@ -14,6 +14,19 @@
 (require 'init-evil)
 (require 'init-company)
 
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
+(defadvice org-html-paragraph (before org-html-paragraph-advice
+                                        (paragraph contents info) activate)
+    "Join consecutive Chinese lines into a single long line without
+unwanted space when exporting org-mode to html."
+    (let* ((origin-contents (ad-get-arg 1))
+           (fix-regexp "[[:multibyte:]]")
+           (fixed-contents
+            (replace-regexp-in-string
+             (concat
+              "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+      (ad-set-arg 1 fixed-contents)))
+
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
 
