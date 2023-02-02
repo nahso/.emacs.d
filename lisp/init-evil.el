131dc8edde2f8de588add03b3ec26fbe13bd9074
duyiming <duyiming22@mails.ucas.ac.cn>
2023-02-02 23:17:45 +0800

    auto update tags

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -10,6 +10,15 @@
           (add-hook 'after-save-hook
             'counsel-etags-virtual-update-tags 'append 'local)))
   :config
+  ;; Don't ask before rereading the TAGS files if they have changed
+  (setq tags-revert-without-query t)
+  ;; Don't warn when TAGS files are large
+  (setq large-file-warning-threshold nil)
+  ;; Setup auto update now
+  (add-hook 'prog-mode-hook
+            (lambda ()
+              (add-hook 'after-save-hook
+                        'counsel-etags-virtual-update-tags 'append 'local)))
   (setq counsel-etags-update-interval 60)
   (push "build" counsel-etags-ignore-directories))
 
