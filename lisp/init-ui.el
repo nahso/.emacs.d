bb5cf2222f6ae6f0cc3383163c9656693fc10593
nahso <topcloud@foxmail.com>
2023-02-28 23:45:42 +0800

    use tree-sitter

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -40,25 +40,27 @@
   :custom
   (highlight-indent-guides-method 'character))
 
-;(use-package tree-sitter
-;  :ensure t
-;  :config
-;  (global-tree-sitter-mode)
-;  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
-;(use-package tree-sitter-langs
-;  :ensure t
-;  :after tree-sitter)
-;(defun load-treesitter()
-;  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.2")
-;  (setq site-lisp-path (expand-file-name "site-lisp/" user-emacs-directory))
-;  (add-to-list 'load-path (concat site-lisp-path "elisp-tree-sitter/core"))
-;  (add-to-list 'load-path (concat site-lisp-path "elisp-tree-sitter/lisp"))
-;  (add-to-list 'load-path (concat site-lisp-path "elisp-tree-sitter/langs"))
-;  (require 'tree-sitter)
-;  (require 'tree-sitter-hl)
-;  (require 'tree-sitter-langs)
-;  (require 'tree-sitter-debug)
-;  (require 'tree-sitter-query))
-;(add-hook 'after-init-hook 'load-treesitter)
+(use-package tree-sitter
+  :ensure t
+  :init
+  (setq tsc-dyn-get-from '(:compilation))
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
+(defun load-treesitter()
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.2")
+  (setq site-lisp-path (expand-file-name "site-lisp/" user-emacs-directory))
+  (add-to-list 'load-path (concat site-lisp-path "elisp-tree-sitter/core"))
+  (add-to-list 'load-path (concat site-lisp-path "elisp-tree-sitter/lisp"))
+  (add-to-list 'load-path (concat site-lisp-path "elisp-tree-sitter/langs"))
+  (require 'tree-sitter)
+  (require 'tree-sitter-hl)
+  (require 'tree-sitter-langs)
+  (require 'tree-sitter-debug)
+  (require 'tree-sitter-query))
+(add-hook 'after-init-hook 'load-treesitter)
 
 (provide 'init-ui)
